c52c6ca8e640f76239c689a5e7cf10a226effd92
iamanas20 <annouslatique@live.fr>
2018-11-14 22:09:00 +0100

    added third (x's and o's coding challenge) and added first method!

---
+++ CodingChallenges/XesAndOes/Manipulator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XesAndOes
+{
+    class Manipulator
+    {
+        /// <summary>
+        /// Determines if the string has both x's and o's
+        /// </summary>
+        /// <param name="input">string to control</param>
+        /// <returns>true if it got em, false if not (none or one of em)</returns>
+        private static bool HasXandO(string input)
+        {
+            bool gotem = false;
+            if (input.ToLower().Contains('x'))
+            {
+                if (input.ToLower().Contains('o'))
+                {
+                    gotem = true;
+                }
+            }
+            return gotem;
+        }
+    }
+}
